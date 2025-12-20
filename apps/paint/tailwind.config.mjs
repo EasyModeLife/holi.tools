b22aca8467958f2eaf3824597e095a215f9a0229
EasyModeLife <easymodelife@users.noreply.github.com>
2025-12-19 18:06:21 -0600

    chore: initial monorepo setup for holi.tools with automation script

---
+++ apps/paint/tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import sharedConfig from "@holi/configs/tailwind.config";
+
+export default {
+    ...sharedConfig,
+    content: [
+        ...sharedConfig.content,
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
+    ],
+};
